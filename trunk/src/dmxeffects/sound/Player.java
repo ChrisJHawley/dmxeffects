a09ce76d417e5f099915ce84604a69581d6afb8c
christopher.hawley <christopher.hawley@4107ba02-ad31-0410-92c7-ad317d56bde2>
2007-05-31 22:54:29 +0000

    Started working on Player class.

---
+++ trunk/src/dmxeffects/sound/Player.java
@@ -0,0 +1,52 @@
+/*
+ * $Id$
+ *
+ * Copyright (C) 2007 Christopher Hawley
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package dmxeffects.sound;
+
+import com.trolltech.qt.core.QObject;
+
+/**
+ * @author chris
+ *
+ */
+public class Player extends QObject implements Runnable {
+
+	public Player() {
+		// TODO Auto-generated method stub
+	}
+	
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	// -- Action handlers -- //
+	public void queueTrack(SoundTrack track) {
+		// TODO Auto-generated method stub
+	}
+	
+	public void play() {
+		// TODO Auto-generated method stub
+	}
+	
+	public void stop() {
+		// TODO Auto-generated method stub
+	}
+
+}
